f3b53c7fc4d96e6eba449e4865d8771e232b1785
tulasinnd <tulasinnd@gmail.com>
2026-04-29 12:27:22 +0530

    chore: add comments to show example chunks

--- src/chunker.py
+++ src/chunker.py
@@ -18,3 +18,28 @@ def chunk_documents(documents, chunk_size, overlap):
             start += chunk_size - overlap
 
     return chunks
+
+'''
+Example chunks:
+
+Total chunks: 5
+
+[{'chunk': 'Transformers are powerful models in NLP.\nThey use attention mechanisms.\nRAG combines retrieval with generation.',
+ 'source': 'sample.txt'}, 
+ 
+ {'chunk': '"You don\'t have to be French to enjoy a decent red wine," Charles Jousselin de Gruse used to tell his foreign 
+ guests whenever he entertained them in Paris. "But you do have to be French to recognize o', 
+ 'source': 'sample1.txt'}, 
+ 
+ {'chunk': 'aris. "But you do have to be French to recognize one," he would add with a laugh.\n\nAfter a lifetime in the French 
+ diplomatic corps, the Count de Gruse lived with his wife in an elegant townhouse on Qu', 
+ 'source': 'sample1.txt'}, 
+ 
+ {'chunk': ' lived with his wife in an elegant townhouse on Quai Voltaire. He was a likeable man, cultivated of course, 
+ with a well-deserved reputation as a generous host and an amusing raconteur.', 
+ 'source': 'sample1.txt'}, 
+ 
+ {'chunk': 'ous host and an amusing raconteur.', 
+ 'source': 'sample1.txt'}]
+
+ '''
